9a8ff5e01ea89376400623223c3324af290f78c7
Artemy <kulbakowork@mail.ru>
2020-11-04 14:53:23 +0300

    Р»Р°Р±Р° 3 РІС‹РїРѕР»РЅРµРЅР°

--- lab3-23.10.20/requests.sql
+++ lab3-23.10.20/requests.sql
@@ -1,13 +1,27 @@
 -- Вариант 1754
 -- 1
-explain select "НАИМЕНОВАНИЕ", "ЧЛВК_ИД" from "Н_ТИПЫ_ВЕДОМОСТЕЙ" тв
+explain analyse select "НАИМЕНОВАНИЕ", "ЧЛВК_ИД" from "Н_ТИПЫ_ВЕДОМОСТЕЙ" тв
     join "Н_ВЕДОМОСТИ" в on в."ТВ_ИД" = тв."ИД"
 where "НАИМЕНОВАНИЕ" < 'Перезачёт' and
     в."ИД" < 1250972;
+
+-- нет смысла строить индекс
+
 -- 2
-explain select л."ИМЯ", в."ДАТА", с."ИД" from "Н_ЛЮДИ" л
+explain analyse select л."ИМЯ", в."ДАТА", с."ИД" from "Н_ЛЮДИ" л
     join "Н_ВЕДОМОСТИ" в on в."ЧЛВК_ИД" = л."ИД"
     join "Н_СЕССИЯ" с on с."ЧЛВК_ИД" = л."ИД"
 where "ФАМИЛИЯ" = 'Афанасьев' and
         в."ЧЛВК_ИД" = 142390 and
         "УЧГОД" = '2001/2002';
+
+create unique index "Н_СЕССИЯ-ЧЛВК_ИД" on "Н_СЕССИЯ" using btree("ЧЛВК_ИД");
+
+/*
+https://habr.com/ru/post/276973/
+https://edu.postgrespro.ru/qpt/qpt_05_bitmapscan.html
+https://postgrespro.ru/docs/postgrespro/10/using-explain
+https://postgrespro.ru/docs/postgresql/13/indexes-types
+https://postgrespro.ru/docs/postgresql/13/sql-createindex
+https://ru.wikipedia.org/wiki/Оптимизация_запросов_СУБД
+*/
